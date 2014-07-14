5398116a76c55c760b85a65bb705bcabd4c9a4a8
Ian Forsey <ian.forsey@net-a-porter.com>
2014-07-14 17:04:58 +0100

    Couldn't get paginate.sh to work yet, so commenting out for now

--- paginate.sh
+++ paginate.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# This script DOES NOT WORK yet!
+
 NUM_OF_SLIDES=$(grep -E 'data-x="[0-9]+"' index.html | wc -l)
 
-for (( i=0; i<=$NUM_OF_SLIDES; i++ ))
+# Replace new line. HACKJOB!
+#perl -p -e 's/\n/NEWLINE/' index.html > index.html.tmp
+
+for (( i=1; i<=$NUM_OF_SLIDES; i++ ))
 do
 	X=$((i * 1500))
-	sed -i '0,/data-x="[0-9]+"/data-x="$x"/' ./index.html
+	#sed -i "" -e "s/data/data$X/$i" index.html
 done
+
+## Put new line back. HACKJOB!
+#perl -p -e 's/NEWLINE/\n/' index.html.tmp > index.html
+# rm index.html.tmp
